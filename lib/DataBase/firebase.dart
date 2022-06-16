f18ae5eff4f5116f9a37824e679ff7706acc719b
Angath Jr <angathjr@gmail.com>
2022-06-16 21:15:11 +0530

    resolved build error

--- lib/DataBase/firebase.dart
+++ lib/DataBase/firebase.dart
@@ -4,15 +4,14 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
- String Username="gig";
- String phone_num="hkguyyu";
- String Email_add="";
+
+String Username = "gig";
+String phone_num = "hkguyyu";
+String Email_add = "";
 FirebaseAuth auth = FirebaseAuth.instance;
 final CollectionReference users =
     FirebaseFirestore.instance.collection('Users');
 final storageRef = FirebaseStorage.instance.ref();
-
-
 
 Future CreateUser(String name, String phone_no) async {
   final docuser =
@@ -25,15 +24,13 @@ Future CreateUser(String name, String phone_no) async {
   await docuser.update(details);
 }
 
-
-Future  readDetails()async{
-  final collection=FirebaseFirestore.instance.collection('Users');
-  final docsnap=await collection.doc(auth.currentUser!.uid).get();
-  if(docsnap.exists){
-    Map<String,dynamic> ? data=docsnap.data();
-    Username =data?['name'];
-    Email_add =data ?['Email_address'];
-    phone_num=data?['mobile_Number'];
-
+Future readDetails() async {
+  final collection = FirebaseFirestore.instance.collection('Users');
+  final docsnap = await collection.doc(auth.currentUser!.uid).get();
+  if (docsnap.exists) {
+    Map<String, dynamic>? data = docsnap.data();
+    Username = data?['name'];
+    Email_add = data?['Email_address'];
+    phone_num = data?['mobile_Number'];
   }
 }
